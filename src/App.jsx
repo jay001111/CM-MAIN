92bc760ef5b76fb839495a14e51a8db01b4ac6bf
ketan-vyam18 <ketan_b231308ec@nitc.ac.in>
2025-03-08 14:02:48 +0530

    web-cm

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  
+
+  return (
+    <>
+    <div id="logo">
+    <img src="./public/WhatsApp_Image_2025-02-28_at_21.50.10_86d30eac-removebg-preview 2.svg"/>
+    <p id="name"><span id="club">CLUB</span> <span>MATHEMATICA</span></p>
+   </div>
+       
+   
+    
+    
+   
+    
+      <div id="nav">
+      
+      <p>Home  </p>
+      
+      
+      <p>Infinitum  </p>
+      
+      
+      <p>Events  </p>
+     
+      
+      <p>Our Team </p>
+    
+      
+      <p>Gallery  </p>
+      
+      
+        <button>
+        About Us
+      </button>
+     
+      
+      <button>
+        Contact Us
+      </button>
+      </div>
+      
+   
+    <img id="page1" src="./public/Component 7.svg"/>
+    
+    <img id="page2" src="./public/Component 5.svg"/>
+   
+    
+
+    <div id="pics">
+    <p id ="description">
+    <b>About C M</b><br/>
+At Club Mathematica, NIT Calicut, we believe that mathematics is more than just numbers—it’s a language of patterns, a tool for innovation, and a bridge between logic and creativity. As a vibrant cultural club, we create a dynamic space where intellectual curiosity meets exhilarating competition.
+
+Through engaging events, challenging puzzles, and thrilling contests, we inspire students to embrace problem-solving, strategic thinking, and the sheer joy of discovery. Our club thrives on the spirit of collaboration, learning, and the relentless pursuit of excellence. Whether you’re a math enthusiast, a puzzle solver, or just someone who loves a good challenge, Club Mathematica is where you belong.
+
+Join us in celebrating the beauty of mathematics beyond textbooks—where logic meets culture, and knowledge turns into an adventure!
+
+Motto: Unravel. Compete. Conquer.
+
+    </p>
+    <div id="infi_flex">
+    <img id="infi" src="./public/Component 4.svg"/>
+    <button id="site">Visit Site</button>
+    </div>
+    </div>
+    
+
+    
+   
+    
+    
+    </>
+  )
+}
+
+export default App
